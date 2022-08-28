47495043e7bb8c50438ee5567710bb94aeb089a3
Tom Dörr <tom.doerr@tum.de>
2022-08-28 22:46:03 +0200

    Update file

--- mui_test/next.config.js
+++ mui_test/next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
+
 const withPWA = require("next-pwa");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-});
+//module.exports = withPWA({
+  //pwa: {
+    //dest: "public",
+    //register: true,
+    //skipWaiting: true,
+  //},
+//});
