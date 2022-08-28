af527858d27220d3de10b1e913e9ebcbd915c2a7
Tom Dörr <tom.doerr@tum.de>
2022-08-28 21:47:50 +0200

    Remove comments

--- mui_test/next.config.js
+++ mui_test/next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-//const nextConfig = {
-  //reactStrictMode: true,
-  //swcMinify: true,
-//}
-
-//module.exports = nextConfig
-
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
